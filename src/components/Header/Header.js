e784eed4c37c2eb5a29f4cb388e5b148db73cbd1
Chukana <chukavina.nv@gmail.com>
2021-01-18 23:56:33 +0400

    doc: make header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,10 +7,16 @@ class Header extends React.Component {
     render() {
         return (
             <div className="Header">
-               Header
+               <ul className="Menu">
+                   <li><a href="#">Home</a></li>
+                   <li><a href="#">Game</a></li>
+                   <li><a href="#">Top chart</a></li>
+               </ul>
+               <img src="/logo.png" alt="logo" className='Logo' width="280px" height='100px'></img>
             </div>
         );
-    }
+
+    }    
 }
 
 export default Header;
