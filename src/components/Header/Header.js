bbb4bed900478a935a4b52735b474e697a336238
Chukana <chukavina.nv@gmail.com>
2021-01-25 08:20:26 +0400

    docs: make a footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,14 +8,13 @@ class Header extends React.Component {
         return (
             <div className="Header">
                <ul className="Menu">
-                   <li><a href="#">Home</a></li>
-                   <li><a href="#">Game</a></li>
-                   <li><a href="#">Top chart</a></li>
+                   <li><a href="/">Home</a></li>
+                   <li><a href="/game">Game</a></li>
+                   <li><a href="/result">Top chart</a></li>
                </ul>
                <img src="/logo.png" alt="logo" className='Logo' width="280px" height='100px'></img>
             </div>
         );
-
     }    
 }
 
