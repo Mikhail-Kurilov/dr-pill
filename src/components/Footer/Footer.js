74f311a50e2df48fd4fd0d75ba90c130a048697b
Chukana <chukavina.nv@gmail.com>
2021-02-03 19:38:36 +0400

    docs: adjust media query

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,35 +17,31 @@ class Footer extends React.Component {
         return (
             <div className="Footer">
                 <table className="table_columns">
-                <tr>
-                    <th> <p className="description">Dr. Pill is the leading interactive entertainment game for people all around the world helping to fight the virus breakdown. We have developed the game for the first time, offering it to enjoy to everyone around the world.</p>
-                    </th>
-                    <th>
-                        <div>
-                        <ul className="block">
-                            <li><a href="/">Home</a></li>
-                            <li><a href="https://github.com/Mikhail-Kurilov">Github_1</a></li>
-                            <li><a href="https://github.com/Chukana">Github_2</a></li>
-                            <li><a href="https://app.rs.school/">RSSchool</a></li>
-                    </ul>
-                    </div>
-                    </th>
-                    <th>
-                        <div>
-                            <ul className="block">
-                                <li><a href="https://www.king.com/game/candycrush">Candy Crash</a></li>
-                                <li><a href="/game">Game</a></li>
-                                <li><a href="/result">Top chart</a></li>
-                            </ul>
-                        </div>
-                    </th>
-                    <th>
-                        <p className="description right">2021   All rights reserved </p>
-                        
-                    </th>
-                </tr>
+                    <tbody>
+                            <tr>
+                            <th> <p className="description">Dr. Pill is the leading interactive entertainment game for people all around the world helping to fight the virus breakdown. We have developed the game for the first time, offering it to enjoy to everyone around the world.</p>
+                            </th>
+                            <th className="links">
+                                    <ul className="block">
+                                        <li><a href="/">Home</a></li>
+                                        <li><a href="https://github.com/Mikhail-Kurilov">Github_1</a></li>
+                                        <li><a href="https://github.com/Chukana">Github_2</a></li>
+                                        <li><a href="https://app.rs.school/">RSSchool</a></li>
+                                    </ul>
+                                    <ul className="block">
+                                        <li><a href="https://www.king.com/game/candycrush">Candy Crash</a></li>
+                                        <li><a href="/game">Game</a></li>
+                                        <li><a href="/result">Top chart</a></li>
+                                        <li><a href="https://en.wikipedia.org/wiki/Super_Mario">Super Mario</a></li>
+                                    </ul>
+                            </th>
+                            <th>
+                                <p className="description right">2021   All rights reserved </p>
+                                
+                            </th>
+                        </tr>
+                    </tbody>
                 </table>
-
             </div>
         );
     }
